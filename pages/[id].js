27839df2170ae6694d7e0bac2e630cd87ff79ea9
DebasishKisan1 <debasish.kisan@gmail.com>
2024-02-17 15:18:24 +0530

    prerendering

--- pages/[id].js
+++ pages/[id].js
@@ -35,4 +35,38 @@ const MeeupDetails = () => {
     )
 }
 
+export const getStaticPaths = () => {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    id: '1'
+                }
+            },
+            {
+                params: {
+                    id: '2'
+                }
+            }
+        ]
+    }
+}
+
+export const getStaticProps = (context) => {
+    const id = context.params.id
+    return {
+        props: {
+            meetupData: {
+                id: id,
+                title: 'first meetup',
+                image: 'https://images.pexels.com/photos/3719037/pexels-photo-3719037.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                address: 'first address',
+                description: 'first description'
+            }
+        }
+    }
+
+}
+
 export default MeeupDetails;
